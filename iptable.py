c3c71511600db72f2773c315d9b65c599cf6b786
satishsinha3922@gmail.com <root@station140.example.com>
2018-01-05 04:45:11 +0530

    gfds

--- iptable.py
+++ iptable.py
@@ -4,24 +4,18 @@ n=int(raw_input("Enter the no of IP to show :"))
 
 #taking out of ip of all system
 result=(commands.getoutput('arp-scan -I wlp6s0 --localnet | grep -i 192.168')).splitlines()
-
 l=len(result)
 
 #define tuple
 y=[]
-#z=[]
 for i in result:
 	y.append(i.split('\t'))
 
 #printing ip list
 print("    IP's are :")
-
 if n <= l:
 	n = n
 else:
 	n = l
-
-#for i in range(0,n):
-#	z.append(y[i][0])
 for i in range(0,n):
 	print("\t\t"+y[i][0]) 
