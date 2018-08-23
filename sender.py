2a802d6b61611ca543c81c3ed7d2483eae8343ad
Satish Kumar <satishsinha3922@gmail.com>
2018-08-23 06:53:21 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,13 +1,11 @@
-mport socket,time
+import socket,time
 
-#s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s=socket.socket()
-
 
 s.connect(("192.168.10.87",1999))
 
 while True:
 	s.send("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii....")
 	time.sleep(2)
-
 s.close()
